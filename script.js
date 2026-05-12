06d07a5e71e20e3fe7c4e6cdd9439d2f382c2bfa
Siluni Jayarathne <silunijayarathne@gmail.com>
2026-05-11 18:30:09 -0700

    new changes

--- script.js
+++ script.js
@@ -9,3 +9,51 @@ projectButton.addEventListener("click", () => {
         });
 
 });
+
+const projectsGrid = document.querySelector(".projects-grid");
+
+const leftArrow = document.getElementById("leftArrow");
+const rightArrow = document.getElementById("rightArrow");
+
+rightArrow.addEventListener("click", () => {
+
+    const maxScroll =
+        projectsGrid.scrollWidth - projectsGrid.clientWidth;
+
+    if (projectsGrid.scrollLeft >= maxScroll - 10) {
+
+        projectsGrid.scrollTo({
+            left: 0,
+            behavior: "smooth"
+        });
+
+    } else {
+
+        projectsGrid.scrollBy({
+            left: 340,
+            behavior: "smooth"
+        });
+
+    }
+
+});
+
+leftArrow.addEventListener("click", () => {
+
+    if (projectsGrid.scrollLeft <= 0) {
+
+        projectsGrid.scrollTo({
+            left: projectsGrid.scrollWidth,
+            behavior: "smooth"
+        });
+
+    } else {
+
+        projectsGrid.scrollBy({
+            left: -340,
+            behavior: "smooth"
+        });
+
+    }
+
+});
